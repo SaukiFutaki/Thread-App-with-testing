333de48240e897b9e2f9ccd8d1e5deb5549b2a6f
sauki futaki <utaki.sauki19@gmail.com>
2024-01-13 19:06:28 +0700

    cek cek

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -18,7 +18,7 @@ export default function LoginPage() {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Login Here!
+                Login Here! ya
               </h1>
               <Logininput login={onLogin} />
               <h1 className="text-white">
